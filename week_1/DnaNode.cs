ccf415e1f6aa4da135e501ad9c589c16cabcf72c
nargeshm99 <nargeshm99@outlook.com>
2019-07-24 16:29:29 +0430

    Do week one assignments

---
+++ week_1/DnaNode.cs
@@ -0,0 +1,27 @@
+﻿namespace A5
+{
+    public class DnaNode
+    {
+        public static int nodesCount=0;
+        public char letter;
+        public int number;
+        public DnaNode parent;
+        public DnaNode[] children = new DnaNode[5];// 0 ->A     1->G    2->T    3->A    4->'0'
+
+        public DnaNode(char letter , DnaNode parent)
+        {
+            this.letter = letter;
+            this.parent = parent;
+            if(letter != '$')
+            this.number = ++nodesCount;
+        }
+
+        public bool IsLeaf ()
+        {
+            if (children[4] != null)
+                return true;
+            return false;
+        }
+
+    }
+}
---
+++ week_1/Q1ConstructTrie.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A5
+{
+    public class Q1ConstructTrie : Processor
+    {
+        public Q1ConstructTrie(string testDataName) : base(testDataName)
+        {
+            this.VerifyResultWithoutOrder = true;
+        }
+
+        public override string Process(string inStr) =>
+        TestTools.Process(inStr, (Func<long, String[], String[]>) Solve);
+
+        public string[] Solve(long n, string[] patterns)
+        {
+            return MakeTrie(patterns);
+        }
+
+        private string[] MakeTrie(string[] patterns)
+        {
+            DnaNode.nodesCount = 0;
+            DnaTri trie = new DnaTri();
+            for(int i =0;i<patterns.Length; i++)
+            {
+                trie.Add(patterns[i]);
+            }
+
+            return MakeArray(trie);
+        }
+
+        private string[] MakeArray(DnaTri trie)
+        {
+            string[] output = new string[trie.nodes.Count];
+            for(int i =1;i<trie.nodes.Count;i++)
+            {
+                if(trie.nodes[i].letter != '$')
+                output[i] = (trie.nodes[i].parent.number-1).ToString() + "->"
+                + (trie.nodes[i].number-1).ToString() + ":" + trie.nodes[i].letter;
+            }
+            Array.Sort(output);
+            return output;
+        }
+    }
+}
