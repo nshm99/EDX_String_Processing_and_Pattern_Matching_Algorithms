0ba47489603e3809e9682525ca896b7de1a94d68
nargeshm99 <nargeshm99@outlook.com>
2019-07-24 16:32:07 +0430

    Do week two assignments

---
+++ week_2/BWt.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A6
+{
+    internal class BWt
+    {
+        public string text;
+        public long[] count = new long[5];//a:0     g:1     c:2     t:3   $:4
+        public long[] startINdex = new long[5];
+        public List<Tuple<int, long,int>> processedArray;//int= number of letter      long = count
+
+        public BWt(string bwt )
+        {
+            this.text = bwt;
+            count[0] = 0;
+            count[1] = 0;
+            count[2] = 0;
+            count[3] = 0;
+            count[4] = 0;
+            startINdex[0] = -1;
+            startINdex[1] = -1;
+            startINdex[2] = -1;
+            startINdex[3] = -1;
+            startINdex[4] = -1;
+            processedArray = new List<Tuple<int, long,int>>();
+            makeArray();
+        }
+
+        private void makeArray()
+        {
+            for(int i = 0; i < text.Length; i++)
+            {
+
+                switch(text[i])
+                {
+                    case 'A':
+                        count[0]++;
+                        processedArray.Add(new Tuple<int, long,int>(0, count[0],i));
+                        if (count[0] == 1)
+                            startINdex[0] = processedArray.Count-1;
+                        break;
+                    case 'G':
+                        count[1]++;
+                        processedArray.Add(new Tuple<int, long,int>(1, count[1],i));
+                        if (count[1] == 1)
+                            startINdex[1] = processedArray.Count - 1;
+                        break;
+                    case 'C':
+                        count[2]++;
+                        processedArray.Add(new Tuple<int, long,int>(2, count[2],i));
+                        if (count[2] == 1)
+                            startINdex[2] = processedArray.Count - 1;
+                        break;
+                    case 'T':
+                        count[3]++;
+                        processedArray.Add(new Tuple<int, long,int>(3, count[3],i));
+                        if (count[3] == 1)
+                            startINdex[3] = processedArray.Count - 1;
+                        break;
+                    case '$':
+                        count[4]++;
+                        processedArray.Add(new Tuple<int, long, int>(4, count[4], i));
+                        if (count[4] == 1)
+                            startINdex[4] = processedArray.Count - 1;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ week_2/Q3MatchingAgainCompressedString.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A6
+{
+    public class Q3MatchingAgainCompressedString : Processor
+    {
+        public Q3MatchingAgainCompressedString(string testDataName) : base(testDataName)
+        {
+        }
+
+        public override string Process(string inStr) => 
+        TestTools.Process(inStr, (Func<String, long, String[], long[]>)Solve);
+
+        public long[] Solve(string text, long n, String[] patterns)
+        {
+            string sortedbwt = Sort(text);
+            BWt first = new BWt(sortedbwt);
+            BWt second = new BWt(text);
+            List<long> result = new List<long>();
+            for(int i=0;i<patterns.Length;i++)
+            {
+                result.Add(BWMatching(first, second,patterns[i].ToList()));
+            }
+            return result.ToArray();
+        }
+
+        private long BWMatching(BWt first, BWt second, List<char> list)
+        {
+            long top = 0;
+            long bottom = second.processedArray.Count()-1;
+            while(top<=bottom)
+            {
+                if (list.Count != 0)
+                {
+                    char symbol = list[list.Count - 1];
+                    int symNum = SymbolNumber(symbol);
+                    list.RemoveAt(list.Count - 1);
+                    Tuple<int, long, int> firstOcc = FindLett(top, bottom, second, symNum);
+                    if (firstOcc.Item1 == -5)
+                        return 0;
+                    top = first.startINdex[firstOcc.Item1] + firstOcc.Item2 - 1;
+                    long countAfterBott = AfterBott(bottom, second, symNum);
+                    bottom = first.startINdex[firstOcc.Item1] + first.count[firstOcc.Item1]-countAfterBott-1;
+
+
+                }
+                else
+                    return bottom - top + 1;
+            }
+            return -4;
+        }
+
+        private long AfterBott(long bottom, BWt second, int symNum)
+        {
+            for(int i=(int)bottom+1;i < second.text.Length;i++)
+            {
+                if (second.processedArray[i].Item1 == symNum)
+                    return second.count[symNum]-second.processedArray[i].Item2+1;
+            }
+            return 0;
+        }
+
+        private int SymbolNumber(char symbol)
+        {
+            int symNum = -2;
+            switch (symbol)
+            {
+                case 'A':
+                    symNum = 0;
+                    break;
+                case 'G':
+                    symNum = 1;
+                    break;
+                case 'C':
+                    symNum = 2;
+                    break;
+                case 'T':
+                    symNum = 3;
+                    break;
+                case '$':
+                    symNum = 4;
+                    break;
+            }
+            return symNum;
+        }
+
+        private Tuple<int, long,int> FindLett(long top, long bottom, BWt second, int symNum)
+        {
+            
+            for(int i=(int)top;i<=(int)bottom;i++)
+            {
+                if(second.processedArray[i].Item1 == symNum)
+                {
+                    return second.processedArray[i];
+                }
+            }
+            return new Tuple<int, long, int>(-5, -5, -5);
+        }
+
+        private string Sort(string bwt)
+        {
+            char[] a = bwt.ToArray();
+            Array.Sort(a);
+            return new string(a);
+        }
+    }
+}
